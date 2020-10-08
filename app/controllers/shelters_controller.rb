2098b08d4e7f581d56f2c0aaa3141e2f10f7e93e
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-08 03:05:55 -0600

    Add shelters#new path

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -7,9 +7,17 @@ class SheltersController < ApplicationController
   end
 
   def create
+    binding.pry
     shelter = Shelter.new({
       name: params[:shelter][:name],
-      
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
       })
+    binding.pry
+    shelter.save
+    binding.pry
+    redirect_to '/shelters'
   end
 end

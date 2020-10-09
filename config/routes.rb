0d1ae626052c26d1a42edd0da8a9026ca03abd03
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-09 01:32:31 -0600

    Add route for pets#new page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
   get '/pets', to: 'pets#index'
+  get '/pets/new', to: 'pets#new'
 end

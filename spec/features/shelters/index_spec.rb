95015249f8b14eab0800050bb73ae660b1ab30fc
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-10 16:39:31 -0600

    Add new shelter-related spec tests to shelter index spec

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -13,11 +13,37 @@ describe "As a user" do
       visit("/shelters")
 
       expect(page).to have_content("All Shelters")
+      expect(page).to have_content("New Shelter")
       expect(page).to have_content("#{shelter1.name}")
       expect(page).to have_content("#{shelter1.address}")
       expect(page).to have_content("#{shelter1.city}, #{shelter1.state} #{shelter1.zip}")
-      expect(page).to have_link("More information")
+      expect(page).to have_content("More information")
+    end
+  end
 
+  describe "when I click this link" do
+    it "More information" do
+      shelter1 = Shelter.create({
+        name: "Bark Bin",
+        address: "4656 Nature Way",
+        city: "McAllen",
+        state: "TX",
+        zip: "75319"
+        })
+      visit("/shelters")
+
+      expect(page).to have_link("More information")
+      click_link("More information")
+      expect(current_path).to eq("/shelters/#{shelter1.id}")
+    end
+
+    it "New Shelter" do
+      visit("/shelters")
+
+      expect(page).to have_link("New Shelter")
+      click_link('New Shelter')
+      expect(current_path).to eq("/shelters/new")
+      expect(page).to have_content("ZIP Code:")
     end
   end
 end

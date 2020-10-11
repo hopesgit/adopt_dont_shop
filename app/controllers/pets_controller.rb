8c7d68e6151e5d44b9699721bc9d25151085cf6c
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-11 17:08:39 -0600

    Add create method to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,4 +5,19 @@ class PetsController < ApplicationController
 
   def new
   end
+
+  def create
+    pet = Pet.new({
+      name: params[:name],
+      description: params[:description],
+      image: params[:image],
+      age: params[:age],
+      sex: params[:sex],
+      adoptability: params[:adoptability]
+      })
+
+    pet.save
+
+    redirect_to '/pets'
+  end
 end

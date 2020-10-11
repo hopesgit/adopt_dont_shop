9669b327735e3dbda732d76802e735d24b78d5ae
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-11 10:58:17 -0600

    Add shelters#edit page

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -23,4 +23,21 @@ class SheltersController < ApplicationController
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+      })
+    shelter.save
+    redirect_to "/shelters/#{shelter.id}"
+  end
 end

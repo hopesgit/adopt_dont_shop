ba90262145e69da82dd55650b58f51ad7b45e2f3
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-11 12:23:38 -0600

    Add route for deleting shelter

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
+  delete 'shelters/:id', to: 'shelters#destroy'
   # Pets
   get '/pets', to: 'pets#index'
   get '/pets/new', to: 'pets#new'

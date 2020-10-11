d887a3aa73b21c12cfacd15e73bcc2afc8084ddf
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-11 07:52:58 -0600

    Add shelters#edit route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+  # Shelters
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  # Pets
   get '/pets', to: 'pets#index'
   get '/pets/new', to: 'pets#new'
 end
